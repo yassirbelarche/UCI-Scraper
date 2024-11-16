3147b2bdfff2dc163dc5aeb540e26f3749197df3
Yassir BELARCHE <117460526+yassirbelarche@users.noreply.github.com>
2024-11-16 16:12:26 +0000

    scrape_uci_datasets

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+
+def scrape_uci_datasets():
+    base_url = "https://archive.ics.uci.edu/datasets"
+
+
+    headers = [
+        "Dataset Name", "Donated Date", "Description",
+        "Dataset Characteristics", "Subject Area", "Associated Tasks",
+        "Feature Type", "Instances", "Features"
+    ]
+
+
+    data = []
