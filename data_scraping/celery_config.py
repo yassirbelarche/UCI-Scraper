a625e102fd13f145bb099d62f1ae27bfc1925259
Yassir BELARCHE <117460526+yassirbelarche@users.noreply.github.com>
2024-11-18 19:27:03 +0000

    correct periodic scheduler

--- data_scraping/celery_config.py
+++ data_scraping/celery_config.py
@@ -1,7 +1,23 @@
 from celery import Celery
 
 # Create the Celery app
-app = Celery('scraper', broker='redis://localhost:6379/0')
+app = Celery('scraper', broker='redis://redis-19273.c339.eu-west-3-1.ec2.redns.redis-cloud.com:19273/0')
+
+# Optional: Set password if required
+app.conf.broker_password = 'fkWeeIquwuNyO0qkj76Nl3NStpph3VKk'
 
 # Optional: set the result backend
-app.conf.result_backend = 'redis://localhost:6379/0'
+# app.conf.result_backend = 'redis://localhost:6379/0'
+
+# Set periodic task schedule (using Celery Beat)
+app.conf.beat_schedule = {
+    'scrape-jobs-every-day': {
+        'task': 'tasks.scrape_job_data',
+        'schedule': 600,  # Every 10 hours
+    },
+}
+app.conf.timezone = 'UTC'
+
+# Start the Celery Worker in terminal 1 with 'celery -A celery_config worker --loglevel=info'
+# Start the Celery Beat Scheduler in terminal 2 with 'celery -A celery_config beat --loglevel=info'
+
